fc1294ebe18df8a0140e10596dab53d08c4428e3
DGalbacheva <dglbacheva@gmail.com>
2024-11-19 10:57:31 +0300

    Added a 100-point yellow view at the bottom and the background of the parent controller colored in red

--- CustomContainerExample/ViewController.swift
+++ CustomContainerExample/ViewController.swift
@@ -8,8 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var contentViewController: UIViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let bottomView = UIView()
+        bottomView.backgroundColor = .yellow
+        bottomView.layer.opacity = 1
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomView)
+        bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        bottomView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        view.backgroundColor = .red
+        
+        
     }
+    
 }
