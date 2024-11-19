4d01b479a025d7d4bfc63528a41b6f2232af5fab
DGalbacheva <dglbacheva@gmail.com>
2024-11-19 10:53:17 +0300

    Created new project for practicing

---
+++ CustomContainerExample/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CustomContainerExample
+//
+//  Created by MacBook Pro 15 on 19.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
